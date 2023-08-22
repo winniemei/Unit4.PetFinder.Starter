e52b26f79fc602a4b85f920c25d2eb95bab4925e
winniemei <wmei91@gmail.com>
2023-08-22 15:00:58 -0400

    add pets

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const pets = require('./data');
 const express = require('express');
 const app = express();
 
-const PORT = 8080;
+const PORT = 8083;
 
 // GET - / - returns homepage
 app.get('/', (req, res) => {
     // serve up the public folder as static index.html file
-
+    res.sendFile(__dirname + '/public/index.html')
 });
 
 // hello world route
@@ -21,31 +21,32 @@ app.get('/api', (req, res) => {
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
-
+    res.send(pets);
 });
 
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
-
+    const {owner} = req.query;
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.owner === owner);
 
     // send the pet as a response
+    res.send(pet);
 
 });
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
-
+    const {name} = req.params;
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
 
     // send the pet as a response
-
+    res.send(pet);
 });
 
 app.listen(PORT, () => {
